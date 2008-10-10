869cf47bbd94cea1c1293067bf60bf96036e8d4a
hackr9483 <hackr9483@38b8cc58-805d-11dd-ac5a-f353d88c4ef8>
2008-10-10 04:03:29 +0000

    Added regular expression stuff to EASM. (TODO: escape regular expressions in C strings)

--- easm/include/easm.h
+++ easm/include/easm.h
@@ -1,7 +1,6 @@
 /*
 <Copyright Header>
-Copyright (c) 2007 - 2008 Jordan "Earlz/hckr83" Earls  
-<http://www.Earlz.biz.tm>
+Copyright (c) 2007 - 2008 Jordan "Earlz/hckr83" Earls  <http://www.Earlz.biz.tm>
 All rights reserved.
 
 Redistribution and use in source and binary forms, with or without
@@ -16,14 +15,12 @@ are met:
 3. The name of the author may not be used to endorse or promote products
    derived from this software without specific prior written permission.
    
-THIS SOFTWARE IS PROVIDED ``AS IS'' AND ANY EXPRESS OR IMPLIED 
-WARRANTIES,
+THIS SOFTWARE IS PROVIDED ``AS IS'' AND ANY EXPRESS OR IMPLIED WARRANTIES,
 INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY
 AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.  IN NO EVENT SHALL
 THE AUTHOR BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
 EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
-PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR 
-PROFITS;
+PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS;
 OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY,
 WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR
 OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF
@@ -34,33 +31,24 @@ This file is part of the EPBP project
 */
 
 
-#include <easm.h>
 
-RegEx reg("gr[ae]y");
+#ifndef EASM_H
+#define EASM_H
+
+#include <iostream>
+#include <regex.h>
+#include <string>
+
+using namespace std;
+
+static const uint32_t REGEX_MAX=64; //maximum number of regular expressions per match(by default)
 
 
-int main(){
-	string pattern;
-	uint32_t i;
-	cout <<"Hi World!"<<endl;
-	cout <<reg.match("grey gray") <<endl;
-	for(i=0;i<=4;i++){
-		cout << reg[i]<<endl;
-	}
-	cout <<"Regular Expression: ";
-	pattern.reserve(256);
-	getline(cin,pattern);
-	RegEx re2(pattern);
-	cout <<endl;
-	cout <<"Search Text: ";
-	getline(cin,pattern);
-	cout <<"Number of matches: "<<re2.match(pattern) <<endl;
-	cout <<"Match List: (new line terminated)" <<endl;
-	for(i=0;i<=re2.number_matches();i++){
-		cout <<re2[i]<<endl;
-	}
-	cout <<"---End of output---"<<endl;
-}
+
+/**Regular Expression Syntax Rules**/
+static const string label_decl_re_pattern="_[a-zA-Z0-9_]*: "; //label declaration
+static const string label_re_pattern="_[a-zA-Z0-9_]* "; //label usage(as a variable)
+static const string register_re_pattern="\s?(?=,)?r[0-9]{1,3}(?=,)?[^[A-Za-z0-9_]"; //register usage
 
 
 
@@ -68,7 +56,43 @@ int main(){
 
 
 
+class RegEx{
+	regex_t re;
+	regmatch_t ma;
+	string *matches;
+	uint32_t matches_limit;
+	bool no_match;
+	
+	public:
+	RegEx(string pattern,int cflags=REG_EXTENDED);
+	~RegEx();
+	string operator[](uint32_t) const;
+	int32_t match(string test,uint32_t max=REGEX_MAX,int flags=0); //returns number of matches
+	void free(); //This will free memory from the last match.
+	uint32_t number_matches();
+};
 
 
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#endif
 
